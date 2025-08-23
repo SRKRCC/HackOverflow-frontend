34b7a563c1aaea47c75e5131be7a6d35834e9b3d
Vigna Ramtej Telagarapu <vignaramtejtelagarapu@gmail.com>
2025-08-23 15:20:56 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar'
+import './App.css'
+import HeroSection from './components/HeroSection'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  )
+}
+
+export default App
