d1dd10f9abc95041a57aacb3f1a8eb4bdbdb28b9
Devireddy Hepsiba <hepsibadevireddy@gmail.com>
2025-09-05 19:28:59 -0600

    Added footer  and about

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar'
 import './App.css'
 import HeroSection from './components/HeroSection'
 import Login from './components/Login'
+import Footer from './components/Footer'
+import About from './components/About'
 
 function AppContent() {
   const location = useLocation()
@@ -11,14 +13,19 @@ function AppContent() {
   return (
     <>
       {!hideNavbar && <Navbar />}
+      
+      
       <Routes>
         <Route path="/" element={<HeroSection />} />
+        
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<div>Register Page</div>} />
         <Route path="/schedule" element={<div>Schedule Page</div>} />
         <Route path="/teams" element={<div>Teams Page</div>} />
         <Route path="/prizes" element={<div>Prizes Page</div>} />
       </Routes>
+      <About/>
+<Footer/>
     </>
   )
 }
