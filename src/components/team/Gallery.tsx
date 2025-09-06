c826f2679f9348693ca4073e1e19090374a14a3a
codevoid048 <williamkeri007@gmail.com>
2025-09-06 19:00:31 +0000

    updated gallery and files moved to team

--- src/components/team/Gallery.tsx
+++ src/components/team/Gallery.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { X, Download } from 'lucide-react';
-import Navbar from './Navbar';
+import Navbar from '../Navbar';
 
 const images = [
   'https://www.gstatic.com/webp/gallery/1.webp',
