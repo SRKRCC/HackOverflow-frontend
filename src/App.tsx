330468736c2ec9a17d4fbd88b64e3d5beb01c500
Ashok-Dd <bonguashok86@gmail.com>
2025-09-09 18:04:10 +0530

    Updated the Home section UI alignments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import './App.css'
-import HeroSection from './components/HeroSection'
 import TeamStructure from './TeamStructure'
 import AdminStructure from './AdminStructure'
 import Login from './components/Login'
@@ -9,6 +8,7 @@ import Register from './components/Register'
 import Footer from './components/Footer'
 import About from './components/About'
 import ProblemStatements from './components/ProblemStatement'
+import Home from './components/Home'
 
 function AppContent() {
   const location = useLocation()
@@ -21,9 +21,8 @@ function AppContent() {
 
       <main className={!hideNavbarFooter ? "pt-16" : ""}>
         <Routes>
-          <Route path="/" element={<HeroSection />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
           <Route path="/register" element={<Register />} />
           <Route path="/problem-statements" element={<ProblemStatements />} />
           <Route path="/schedule" element={<div>Schedule Page</div>} />
